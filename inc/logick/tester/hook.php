4a1887f8ad8157ae7e601a895da206f6f55d7397
Sergey I. Sharybin <g.ulairi@gmail.com>
2009-03-21 16:17:31 +0500

    * Fixed bug with OnUserDelete hook.

--- inc/logick/tester/hook.php
+++ inc/logick/tester/hook.php
@@ -1,13 +1,17 @@
 <?php if ($_WT_hook_included_!='##WT_Hook_Included##') {$_WT_hook_included_='##WT_Hook_Included##';
 
   function WT_on_user_delete ($user_id) {
-     WT_delete_solution_from_xpfs ("`user_id`=$user_id");
-     db_delete ('tester_solutions', "`user_id`=$user_id");
+    /* util.php is not included yset, so we need this stupid code here */
+    global $XPFS;
+    $q=db_select ('tester_solutions', array ('id'), "`user_id`=$user_id");
+    while ($r=db_row ($q)) $XPFS->removeItem ('/tester/testing/'.$r['id']);
+
+    db_delete ('tester_solutions', "`user_id`=$user_id");
   }
 
   function WT_on_group_delete ($group_id) {
-     db_delete ('tester_contestgroup', "`group_id`=$group_id");
-     db_delete ('tester_contestjudge', "`group_id`=$group_id");
+    db_delete ('tester_contestgroup', "`group_id`=$group_id");
+    db_delete ('tester_contestjudge', "`group_id`=$group_id");
   }
 
   hook_register ('CORE.Security.OnUserDelete',  WT_on_user_delete);
