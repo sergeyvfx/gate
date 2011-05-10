eb983515979bbe63b36d0d2221fcd456e8c7099a
Alexander O. Anisimov <alenyashka@gmail.com>
2011-05-10 14:43:20 +0600

    Add capability to parse args

--- tools/tableChecker/src/tablechecker/TableChecker.java
+++ tools/tableChecker/src/tablechecker/TableChecker.java
@@ -1,16 +1,43 @@
 package tablechecker;
 
-import java.io.IOException;
-import tablechecker.core.Parser;
+import java.io.File;
 
 public class TableChecker {
 
   public static void main(String[] args) {
-    Parser p = new Parser("1.html");
-    try {
-      p.parse();
-    } catch (IOException ex) {
-      ex.printStackTrace();
+    String jarFile = TableChecker.class.getProtectionDomain().
+            getCodeSource().getLocation().getPath();
+    if (args.length != 2) {
+      System.out.println("Usage: java -jar " + jarFile + " fileToCheck dirWithTemplates");
+      return;
+    }
+
+    File fileToCheck = new File(args[0]);
+    if (!fileToCheck.exists()) {
+      System.out.println("File " + fileToCheck.getPath() + " not exists!");
+      return;
+    }
+    if (!fileToCheck.isFile()) {
+      System.out.println(fileToCheck.getPath() + " is not a file!");
+      return;
+    }
+    if (!fileToCheck.canRead()) {
+      System.out.println("File " + fileToCheck.getPath() + " can not be read!");
+      return;
+    }
+
+    File templatesDir = new File(args[1]);
+    if (!templatesDir.exists()) {
+      System.out.println("Directory " + templatesDir.getPath() + " not exists!");
+      return;
+    }
+    if (!templatesDir.isDirectory()) {
+      System.out.println(templatesDir.getPath() + " is not a file!");
+      return;
+    }
+    if (!templatesDir.canRead()) {
+      System.out.println("Directory " + templatesDir.getPath() + " can not be read!");
+      return;
     }
   }
 }
