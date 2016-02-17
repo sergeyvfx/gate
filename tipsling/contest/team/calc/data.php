46dd0368b99097b816d0a68ebc2b86e7fbe72103
keeper <keeperami@gmail.com>
2016-02-17 23:41:38 +0500

    fogotten change for new type of discount

--- tipsling/contest/team/calc/data.php
+++ tipsling/contest/team/calc/data.php
@@ -49,7 +49,7 @@ ${information}
     ?>
       <br/>
       Выберите применяемые скидки:</br>
-      <input type="checkbox" id="repost" value="200">[200р]Скидка за распространение информации о конкурсе (не менее 10 сообщений о конкурсе в сети)</br>
+      <input type="checkbox" id="repost" value="100">[100р]Скидка за распространение информации о конкурсе (не менее 10 сообщений о конкурсе в сети)</br>
       <input type="checkbox" id="early" value="100">[100р]Скидка за раннюю оплату (до 1 марта)</br>
       <input type="checkbox" id="years" value="100">[100р]Скидка за возраст (для команд с 1 по 9 класс)</br>
       <input type="checkbox" id="participant" value="100">[100р]Скидка участникам предыдущих конкурсов (хотя бы один из учеников уже принимал участие в конкурсе)</br>
@@ -57,6 +57,7 @@ ${information}
       <input type="checkbox" id="winer" value="100">[100р]Скидка призерам предыдущих конкурсов (хотя бы один из учеников занимал призовое место в одном из предыдущих конкурсов)</br>
       <input type="checkbox" id="teacher_participant" value="100">[100р]Скидка учителям-участникам прежних конкурсов</br>
       <input type="checkbox" id="teacher_winer" value="100">[100р]Скидка учителям-победителям прежних конкурсов</br>
+      <input type="checkbox" id="other_contest" value="100">[100р]Скидка за участие в других конкурсах по ТРИЗ</br>
       </br>
       </br>
       Суммарная скидка: <input type="text" id="discount" readonly="readonly" value="0"/>
